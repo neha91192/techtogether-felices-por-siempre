b8fdf876eefd7881a9dfff61c0932edde87e73d1
Neha <shukla.ne@husky.neu.edu>
2019-03-22 23:09:58 -0400

    added login and register

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import {Route} from "react-router";
+
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                {"Hello"}
-            </div>
+            <Router>
+                <div className="App">
+                    <Route exact path="/login"
+                           component={Login}>
+                    </Route>
+                    <Route exact path="/register"
+                           component={Register}>
+                    </Route>
+                </div>
+            </Router>
         );
     }
 }
---
+++ src/Components/Register.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+export default class Register extends React.Component {
+    render() {
+        return (
+            <div>
+                <form className="text-center border border-light p-5">
+                    <h1>Sign Up</h1>
+                    <input type="text" className="form-control mb-4" placeholder="Email"/>
+                    <input type="password" id="defaultLoginFormPassword" className="form-control mb-4"
+                           placeholder="Password"/>
+                    <input type="password" id="defaultLoginFormPassword" className="form-control mb-4"
+                           placeholder="Confirm Password"/>
+                    <button className="btn btn-dark btn-block my-4" type="button">Continue</button>
+                </form>
+            </div>
+        );
+    }
+}
