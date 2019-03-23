2a8af6dc93a137a9a2230869cb5520c8f23649a6
Sarada Symonds <symonds.s@husky.neu.edu>
2019-03-23 02:15:51 -0400

    Added simple navigation bar with links for logged in and logged out state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NavBar from './Components/Header/NavBar';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        user_id: 1
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <NavBar user_id={this.state.user_id} is_child={false} />
       </div>
     );
   }
