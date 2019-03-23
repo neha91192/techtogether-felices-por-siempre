c22f7ad3b0cdfa416c9305182e4fbe13a92d0c82
Neha <shukla.ne@husky.neu.edu>
2019-03-22 23:15:32 -0400

    removed backend

--- backend/felices/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Child
-
-class ChildSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Child
-        fields = ('id', 'name', 'last_seen', 'found')
--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';       // add this
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
